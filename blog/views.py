a91b5c1665d5d1d19afa9bf9c1c327fa5b337fbd
audio35444 <juaninsis@gmail.com>
2017-03-10 08:33:28 -0300

    Modified using template, asociate view with models

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-	return render(request,'blog\post_list.html', {})
+	qs_post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	cant = xrange(len(qs_post))
+	return render(request,'blog\post_list.html', {'qs_post':qs_post,'cant':cant})
